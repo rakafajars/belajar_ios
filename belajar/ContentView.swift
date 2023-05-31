0de8a27c08537b6b791ec0edf7491194697c97ff
raka <rakafajar18@gmail.com>
2023-06-01 06:01:07 +0700

    add project belejar ios

--- belajar/ContentView.swift
+++ belajar/ContentView.swift
@@ -6,83 +6,64 @@
 //
 
 import SwiftUI
-import CoreData
+
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        ZStack {
+            Image("bg").resizable().edgesIgnoringSafeArea(.all)
+            VStack(spacing:20){
+                Logo()
+                FormBox()
+            }.padding(.all,20)
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+                ContentView()
+    }
+}
+
+struct Logo: View {
+    var body: some View {
+        VStack(spacing:20){
+            Image("icon").resizable().frame(
+                width: 80, height: 80).foregroundColor(Color.white).padding().cornerRadius(80)
+            
+            Text("Hello SwiftUI").foregroundColor(Color.black).background(Color.white)
+            
+        }
+    }
+}
+
+
+struct FormBox: View {
+    
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    var body: some View {
+        VStack(alignment:.leading) {
+            Text("Username").font(.callout).bold()
+            TextField("Username...", text:$username).textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Password").font(.callout).bold()
+            SecureField("Password...", text:$password).textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                print("Helo Button")
+            }) {
+                HStack{
+                    Text("Sign In")
+                    Spacer()
+                }
+            }.padding().background(Color.black).cornerRadius(10).foregroundColor(Color.white)
+
+        }.padding(.all, 30).background(Color.orange).cornerRadius(10)
+        
+        
     }
 }
