dc5adcb0583beedfa6d8cdbf13f031340fb547cf
raka <rakafajar18@gmail.com>
2023-06-01 09:40:52 +0700

    Form dan Navigation

--- belajar/ContentView.swift
+++ belajar/ContentView.swift
@@ -10,12 +10,84 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("bg").resizable().edgesIgnoringSafeArea(.all)
-            VStack(spacing:20){
-                Logo()
-                FormBox()
-            }.padding(.all,20)
+        NavigationView {
+            Form{
+                // Section Photo Profile
+                Section() {
+                    NavigationLink(destination: About())
+                    {
+                        HStack{
+                            Image("bg").resizable()
+                                .frame(width: 50, height: 50)
+                                .clipShape(Circle())
+                            
+                            // Nama Dan Status
+                            VStack(alignment: .leading){
+                                Text("Raka Fajar Salinggih").font(.headline)
+                                Text("Ios Developer").font(.caption)
+                            }
+                        }.padding(.top, 10).padding(.bottom,10)
+                    }                                    }
+                
+                // Section Pengaturan Umum
+                Section(header: Text("Pengaturan Umum")){
+                    
+                    NavigationLink(destination: About())
+                    {
+                        HStack(spacing:20){
+                            Image(systemName: "star.fill")
+                                .frame(width: 35, height: 35)
+                                .background(Color.orange)
+                                .cornerRadius(10)
+                                .foregroundColor(Color.white)
+                            Text("Pesan Berbintang")
+                        }
+                    }
+                    
+                    NavigationLink(destination: About())
+                    {
+                        HStack(spacing:20){
+                            Image(systemName: "tv")
+                                .frame(width: 35, height: 35)
+                                .background(Color.green)
+                                .cornerRadius(10)
+                                .foregroundColor(Color.white)
+                            Text("WhatsApp Web/Desktop")
+                        }
+                    }
+                    
+                }
+                
+                // Pengaturan Akun
+                Section(header: Text("Pengaturan Akun")){
+                    // Akun
+                    NavigationLink(destination: About()) {
+                        HStack(spacing:20){
+                                Image(systemName: "person")
+                                .frame(width: 35, height: 35)
+                                .background(Color.blue)
+                                .foregroundColor(Color.white)
+                                .cornerRadius(10)
+                                Text("Akun")
+                        }
+                    }
+                    
+                    // Chat
+                    NavigationLink(destination: About()) {
+                        HStack(spacing:20){
+                                Image(systemName: "phone.circle")
+                                .frame(width: 35, height: 35)
+                                .background(Color.green)
+                                .foregroundColor(Color.white)
+                                .cornerRadius(10)
+                                Text("Chat")
+                        }
+
+                    }
+                    
+                }
+                
+            }.navigationBarTitle("Setting")
         }
     }
 }
@@ -27,43 +99,9 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Logo: View {
+
+struct About : View {
     var body: some View {
-        VStack(spacing:20){
-            Image("icon").resizable().frame(
-                width: 80, height: 80).foregroundColor(Color.white).padding().cornerRadius(80)
-            
-            Text("Hello SwiftUI").foregroundColor(Color.black).background(Color.white)
-            
-        }
-    }
-}
-
-
-struct FormBox: View {
-    
-    
-    @State var username: String = ""
-    @State var password: String = ""
-    var body: some View {
-        VStack(alignment:.leading) {
-            Text("Username").font(.callout).bold()
-            TextField("Username...", text:$username).textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Text("Password").font(.callout).bold()
-            SecureField("Password...", text:$password).textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Button(action: {
-                print("Helo Button")
-            }) {
-                HStack{
-                    Text("Sign In")
-                    Spacer()
-                }
-            }.padding().background(Color.black).cornerRadius(10).foregroundColor(Color.white)
-
-        }.padding(.all, 30).background(Color.orange).cornerRadius(10)
-        
-        
+        Text("Halo View")
     }
 }
