6858f97e68b5b9d8ad80086070f24da7136f236c
raka <rakafajar18@gmail.com>
2023-06-04 13:16:48 +0700

    belajar membuat appbar dan list

--- belajar/ContentView.swift
+++ belajar/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+<<<<<<< Updated upstream
         ZStack {
             Image("bg").resizable().edgesIgnoringSafeArea(.all)
             VStack(spacing:20){
@@ -17,16 +18,20 @@ struct ContentView: View {
                 FormBox()
             }.padding(.all,20)
         }
+=======
+        Home()
+>>>>>>> Stashed changes
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-                ContentView()
+        ContentView()
     }
 }
 
+<<<<<<< Updated upstream
 struct Logo: View {
     var body: some View {
         VStack(spacing:20){
@@ -36,10 +41,49 @@ struct Logo: View {
             Text("Hello SwiftUI").foregroundColor(Color.black).background(Color.white)
             
         }
+=======
+
+struct Home: View {
+    var body: some View {
+        NavigationView {
+            Konten().navigationBarItems(
+                    leading: HStack{
+                        Button(action:{
+                            print("hello")
+                        }) {
+                            Image("yt").resizable().renderingMode(.original).frame(width: 40, height: 40)
+                            
+                        }
+                    },
+                    trailing: HStack {
+                        Button(action: {print("helo")}) {
+                            Image(systemName: "tray.full").foregroundColor(Color.secondary)
+                        }
+                        
+                        Button(action: {print("helo")}) {
+                            Image(systemName: "video.fill").foregroundColor(Color.secondary)
+                            
+                        }
+                        
+                        Button(action: {print("helo")}) {
+                            Image(systemName: "magnifyingglass").foregroundColor(Color.secondary)
+                            
+                        }
+                        
+                        Button(action: {print("helo")}) {
+                            Image("bg").renderingMode(.original).resizable().frame(width: 45,height: 45).clipShape(Circle())
+                            
+                        }
+                        
+                    }
+            ).navigationBarTitle("", displayMode: .inline)
+        }.navigationViewStyle(StackNavigationViewStyle())
+>>>>>>> Stashed changes
     }
 }
 
 
+<<<<<<< Updated upstream
 struct FormBox: View {
     
     
@@ -65,5 +109,78 @@ struct FormBox: View {
         }.padding(.all, 30).background(Color.orange).cornerRadius(10)
         
         
+=======
+
+struct Konten : View {
+    var body: some View {
+        List{
+            
+            // konten 1
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image("content1").resizable().aspectRatio(contentMode: ContentMode.fill)
+                    Text("10:00")
+                        .padding(.all, 5)
+                        .foregroundColor(Color.white)
+                        .font(.caption)
+                        .background(Color.black)
+                        .cornerRadius(5)
+                        .padding(.trailing, 10)
+                        .padding(.bottom, 5)
+                }
+                
+                HStack(spacing:20) {
+                    Image("bg")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .clipShape(Circle())
+                    
+                    VStack(alignment: .leading) {
+                        Text("Belajar SWIFTUI Bagian Pertama")
+                            .font(.headline)
+                        HStack {
+                            Text("Noiz Chanel - 300x kamu pasti bisa").font(.caption)
+                        }
+                    }
+                    Spacer()
+                    Image(systemName: "list.bullet")
+
+                }
+            }
+            
+            
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image("content1").resizable().aspectRatio(contentMode: ContentMode.fill)
+                    Text("10:00")
+                        .padding(.all, 5)
+                        .foregroundColor(Color.white)
+                        .font(.caption)
+                        .background(Color.black)
+                        .cornerRadius(5)
+                        .padding(.trailing, 10)
+                        .padding(.bottom, 5)
+                }
+                
+                HStack(spacing:20) {
+                    Image("bg")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .clipShape(Circle())
+                    
+                    VStack(alignment: .leading) {
+                        Text("Belajar SWIFTUI Bagian Pertama")
+                            .font(.headline)
+                        HStack {
+                            Text("Noiz Chanel - 300x kamu pasti bisa").font(.caption)
+                        }
+                    }
+                    Spacer()
+                    Image(systemName: "list.bullet")
+
+                }
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
