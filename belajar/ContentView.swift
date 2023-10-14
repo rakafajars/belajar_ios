b2a1294d92f9341f19c951d01fa9e5412f186745
raka fajar <rakafajar18@gmail.com>
2023-10-14 22:47:48 +0700

    belajar tab view

--- belajar/ContentView.swift
+++ belajar/ContentView.swift
@@ -10,7 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Home()
+        ZStack {
+            TabView {
+                Home().tabItem{
+                    Image(systemName: "house.fill")
+                    Text("Beranda")
+                }
+                Home().tabItem{
+                    Image(systemName: "paperplane.fill")
+                    Text("Explorasi")
+                }
+                
+                Home().tabItem{
+                    Image(systemName: "tray.fill")
+                    Text("Subscription")
+                }
+                Home().tabItem{
+                    Image(systemName: "envelope.fill")
+                    Text("Kotak Masuk")
+                }
+                Home().tabItem{
+                    Image(systemName: "play.rectangle.fill")
+                    Text("Koleksi")
+                }
+            }.accentColor(.red)
+        }
     }
 }
 
@@ -26,35 +50,35 @@ struct Home: View {
     var body: some View {
         NavigationView {
             Konten().navigationBarItems(
-                    leading: HStack{
-                        Button(action:{
-                            print("hello")
-                        }) {
-                            Image("yt").resizable().renderingMode(.original).frame(width: 40, height: 40)
-                            
-                        }
-                    },
-                    trailing: HStack {
-                        Button(action: {print("helo")}) {
-                            Image(systemName: "tray.full").foregroundColor(Color.secondary)
-                        }
-                        
-                        Button(action: {print("helo")}) {
-                            Image(systemName: "video.fill").foregroundColor(Color.secondary)
-                            
-                        }
-                        
-                        Button(action: {print("helo")}) {
-                            Image(systemName: "magnifyingglass").foregroundColor(Color.secondary)
-                            
-                        }
-                        
-                        Button(action: {print("helo")}) {
-                            Image("bg").renderingMode(.original).resizable().frame(width: 45,height: 45).clipShape(Circle())
-                            
-                        }
+                leading: HStack{
+                    Button(action:{
+                        print("hello")
+                    }) {
+                        Image("yt").resizable().renderingMode(.original).frame(width: 40, height: 40)
                         
                     }
+                },
+                trailing: HStack {
+                    Button(action: {print("helo")}) {
+                        Image(systemName: "tray.full").foregroundColor(Color.secondary)
+                    }
+                    
+                    Button(action: {print("helo")}) {
+                        Image(systemName: "video.fill").foregroundColor(Color.secondary)
+                        
+                    }
+                    
+                    Button(action: {print("helo")}) {
+                        Image(systemName: "magnifyingglass").foregroundColor(Color.secondary)
+                        
+                    }
+                    
+                    Button(action: {print("helo")}) {
+                        Image("bg").renderingMode(.original).resizable().frame(width: 45,height: 45).clipShape(Circle())
+                        
+                    }
+                    
+                }
             ).navigationBarTitle("", displayMode: .inline)
         }.navigationViewStyle(StackNavigationViewStyle())
     }
@@ -65,71 +89,75 @@ struct Home: View {
 struct Konten : View {
     var body: some View {
         List{
+            CellKonten(imageKontent: "content1",
+                       profileKontent: "bg",
+                       judul: "Belajar SwiftUI", deskripsi:
+                        "Belajar Adalah Kekuatan",
+                       durasi: "10:00")
             
-            // konten 1
-            VStack {
-                ZStack(alignment: .bottomTrailing) {
-                    Image("content1").resizable().aspectRatio(contentMode: ContentMode.fill)
-                    Text("10:00")
-                        .padding(.all, 5)
-                        .foregroundColor(Color.white)
-                        .font(.caption)
-                        .background(Color.black)
-                        .cornerRadius(5)
-                        .padding(.trailing, 10)
-                        .padding(.bottom, 5)
-                }
-                
-                HStack(spacing:20) {
-                    Image("bg")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .clipShape(Circle())
-                    
-                    VStack(alignment: .leading) {
-                        Text("Belajar SWIFTUI Bagian Pertama")
-                            .font(.headline)
-                        HStack {
-                            Text("Noiz Chanel - 300x kamu pasti bisa").font(.caption)
-                        }
-                    }
-                    Spacer()
-                    Image(systemName: "list.bullet")
+            CellKonten(imageKontent: "content2",
+                       profileKontent: "bg",
+                       judul: "Belajar Matematika", deskripsi:
+                        "Belajar Adalah Hebat Kamu Bisa",
+                       durasi: "15:00")
+            
+            CellKonten(imageKontent: "content1",
+                       profileKontent: "bg",
+                       judul: "Belajar SwiftUI", deskripsi:
+                        "Belajar Adalah Kekuatan",
+                       durasi: "10:00")
+            
+            CellKonten(imageKontent: "content2",
+                       profileKontent: "bg",
+                       judul: "Belajar Matematika", deskripsi:
+                        "Belajar Adalah Hebat Kamu Bisa",
+                       durasi: "15:00")
 
-                }
+        }
+    }
+}
+
+
+
+// komponen untuk cell
+struct CellKonten : View {
+    // parameter variabel
+    var imageKontent: String
+    var profileKontent: String
+    var judul: String
+    var deskripsi: String
+    var durasi: String
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image(imageKontent).resizable().aspectRatio(contentMode: ContentMode.fill)
+                Text(durasi)
+                    .padding(.all, 5)
+                    .foregroundColor(Color.white)
+                    .font(.caption)
+                    .background(Color.black)
+                    .cornerRadius(5)
+                    .padding(.trailing, 10)
+                    .padding(.bottom, 5)
             }
             
-            
-            VStack {
-                ZStack(alignment: .bottomTrailing) {
-                    Image("content1").resizable().aspectRatio(contentMode: ContentMode.fill)
-                    Text("10:00")
-                        .padding(.all, 5)
-                        .foregroundColor(Color.white)
-                        .font(.caption)
-                        .background(Color.black)
-                        .cornerRadius(5)
-                        .padding(.trailing, 10)
-                        .padding(.bottom, 5)
-                }
+            HStack(spacing:20) {
+                Image(profileKontent)
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .clipShape(Circle())
                 
-                HStack(spacing:20) {
-                    Image("bg")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .clipShape(Circle())
-                    
-                    VStack(alignment: .leading) {
-                        Text("Belajar SWIFTUI Bagian Pertama")
-                            .font(.headline)
-                        HStack {
-                            Text("Noiz Chanel - 300x kamu pasti bisa").font(.caption)
-                        }
+                VStack(alignment: .leading) {
+                    Text(judul)
+                        .font(.headline)
+                    HStack {
+                        Text(deskripsi).font(.caption)
                     }
-                    Spacer()
-                    Image(systemName: "list.bullet")
-
                 }
+                Spacer()
+                Image(systemName: "list.bullet")
+                
             }
         }
     }
